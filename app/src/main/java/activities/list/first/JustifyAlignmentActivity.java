5bc5eddc55afd5ba8775e6dbf544e753ce826fc2
pankajnimgade <pankaj.nimgade@gmail.com>
2016-01-05 10:04:29 +0530

    added more

--- app/src/main/java/activities/list/first/JustifyAlignmentActivity.java
+++ app/src/main/java/activities/list/first/JustifyAlignmentActivity.java
@@ -3,21 +3,15 @@ package activities.list.first;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.widget.TextView;
+import android.webkit.WebView;
 
 import com.nimgade.pk.mytutorialapplication.R;
 
+/**
+ * This activity gives you an idea about how to apply justify alignment to the TextView
+ */
 public class JustifyAlignmentActivity extends AppCompatActivity {
 
-    private String some_random_text = "If you really want to learn, and you want to learn more quickly and more deeply,\n" +
-            "pay attention to how you pay attention. Think about how you think, Learn how\n" +
-            "you learn.\n\n" +
-            "Most of us did not take courses on metacognition or learning theory when we were\n" +
-            "growing up. We were expected to learn, but rarely taught to learn.\n" +
-            "But we assume that ifyou're holding this book, you want to learn Java. And you\n" +
-            "probably don't want to spend a lot of time.";
-
-    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,8 +27,26 @@ public class JustifyAlignmentActivity extends AppCompatActivity {
     }
 
     private void initializeUI() {
-        textView = (TextView) findViewById(R.id.JustifyAlignmentActivity_textView);
-        textView.setText("" + some_random_text);
+        /**
+         * This is the TextView to which you would be implementing justify alignment
+         */
+        WebView webView = (WebView) findViewById(R.id.JustifyAlignmentActivity_textView);
+
+        /**
+         * pick some random text, I took it from Head First Java
+         */
+        String some_random_text = "If you really want to learn, and you want to learn more quickly and more deeply,\n" +
+                "pay attention to how you pay attention. Think about how you think, Learn how\n" +
+                "you learn.\n\n" +
+                "Most of us did not take courses on metacognition or learning theory when we were\n" +
+                "growing up. We were expected to learn, but rarely taught to learn.\n" +
+                "But we assume that ifyou're holding this book, you want to learn Java. And you\n" +
+                "probably don't want to spend a lot of time.";
+
+        String text = "<html><body>" + "<p align=\"justify\" >";
+        text += some_random_text;
+        text += "</body></html>";
+        webView.loadData(text, "text/html; charset=UTF-8", null);
     }
 
     @Override
