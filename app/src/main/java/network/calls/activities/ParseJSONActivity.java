ab35e5a79c983e15220b12b021f902a7184b7e9a
pankajnimgade <pankaj.nimgade@gmail.com>
2016-01-27 10:15:29 +0530

    parsing json data completed

--- app/src/main/java/network/calls/activities/ParseJSONActivity.java
+++ app/src/main/java/network/calls/activities/ParseJSONActivity.java
@@ -1,13 +1,20 @@
 package network.calls.activities;
 
+import android.content.Context;
+import android.graphics.Color;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import com.nimgade.pk.mytutorialapplication.R;
 
@@ -19,11 +26,19 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.ArrayList;
 
+/**
+ * { http://stackoverflow.com/questions/35004874/json-parsing-from-database }
+ */
 public class ParseJSONActivity extends AppCompatActivity {
 
     private Button button;
+    private Button previous_Button;
+    private Button next_Button;
+
     private ListView listView;
     private ArrayList<People> peoples;
+    private MyAdapter adapter;
+    private int TRACK = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,7 +55,66 @@ public class ParseJSONActivity extends AppCompatActivity {
     private void initializeUI() {
 
         button = (Button) findViewById(R.id.ParseJSONActivity_ok_button);
+        previous_Button = (Button) findViewById(R.id.ParseJSONActivity_Previous_button);
+        previous_Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (peoples != null) {
+                    if (peoples.size() > 0) {
+                        if ((TRACK < peoples.size() - 1) && (TRACK >= 1)) {
+                            People people = (People) listView.getItemAtPosition(--TRACK);
+                            for (People single_item : peoples) {
+                                single_item.setSelected(false);
+                            }
+                            people.setSelected(true);
+                            adapter.notifyDataSetChanged();
+                        }else{
+                            TRACK = 0;
+                        }
+                    }
+                }
+            }
+        });
+        next_Button = (Button) findViewById(R.id.ParseJSONActivity_next_button);
+        next_Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (peoples != null) {
+                    if (peoples.size() > 0) {
+                        if (TRACK < peoples.size() - 2) {
+                            People people = (People) listView.getItemAtPosition(++TRACK);
+                            for (People single_item : peoples) {
+                                single_item.setSelected(false);
+                            }
+                            people.setSelected(true);
+                            adapter.notifyDataSetChanged();
+                        }else {
+                            System.out.println("TRACK: "+TRACK);
+                            TRACK = 0;
+                        }
+                    }
+                }else{
+                    System.out.println("peoples is null");
+                }
+            }
+        });
         listView = (ListView) findViewById(R.id.ParseJSONActivity_listView);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                if (peoples != null) {
+                    if (peoples.size() > 0) {
+                        TRACK = position;
+                        People people = (People) listView.getItemAtPosition(position);
+                        for (People single_item : peoples) {
+                            single_item.setSelected(false);
+                        }
+                        people.setSelected(true);
+                        adapter.notifyDataSetChanged();
+                    }
+                }
+            }
+        });
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -93,13 +167,59 @@ public class ParseJSONActivity extends AppCompatActivity {
             super.onPostExecute(aVoid);
             if (peoples != null) {
                 if (peoples.size() > 0) {
-                    ArrayAdapter<People> adapter =
-                            new ArrayAdapter<People>(getApplicationContext(), R.layout.simple_list_item_1, peoples);
+                    adapter = new MyAdapter(getApplicationContext(), R.layout.single_item_custom_one, peoples);
                     listView.setAdapter(adapter);
-
+                    adapter.notifyDataSetChanged();
                 }
             }
         }
+    }
+
+    private class MyAdapter extends ArrayAdapter {
+
+        private ArrayList<People> a_productInfos;
+        private Context a_context;
+        private LayoutInflater a_layoutInflater;
+
+        public MyAdapter(Context context, int resource, ArrayList<People> a_productInfos) {
+            super(context, resource, a_productInfos);
+            this.a_productInfos = a_productInfos;
+            this.a_context = context;
+            a_layoutInflater = LayoutInflater.from(this.a_context);
+        }
+
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            View row = convertView;
+            ViewHolder holder = null;
+            if (row == null) {
+                row = a_layoutInflater.inflate(R.layout.single_item_custom_one, parent, false);
+                holder = new ViewHolder();
+                holder.product_name = (TextView) row.findViewById(R.id.single_item_custom_one_textView);
+                holder.item_LinearLayout = (LinearLayout) row.findViewById(R.id.single_item_custom_one_linearLayout);
+                row.setTag(holder);
+            } else {
+                holder = (ViewHolder) row.getTag();
+            }
+
+            final People productInfo = a_productInfos.get(position);
+            holder.product_name.setText("" + productInfo.getName());
+
+            if (productInfo.isSelected) {
+                holder.item_LinearLayout.setBackgroundColor(Color.parseColor("#ff44ff"));
+            } else {
+                holder.item_LinearLayout.setBackgroundColor(Color.parseColor("#ffffff"));
+            }
+
+            return row;
+        }
+
+        class ViewHolder {
+            TextView product_name;
+            LinearLayout item_LinearLayout;
+        }
+
     }
 
     private class People {
@@ -107,6 +227,7 @@ public class ParseJSONActivity extends AppCompatActivity {
         private String Name;
         private String profession;
         private String image;
+        boolean isSelected;
 
         public String getId() {
             return id;
@@ -140,9 +261,12 @@ public class ParseJSONActivity extends AppCompatActivity {
             this.image = image;
         }
 
-        @Override
-        public String toString() {
-            return Name;
+        public boolean isSelected() {
+            return isSelected;
+        }
+
+        public void setSelected(boolean selected) {
+            isSelected = selected;
         }
     }
 
