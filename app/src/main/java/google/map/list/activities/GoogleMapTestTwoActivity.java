dec42a57ee0a1ff2aaeb4a2042f3b1dd431e91da
pankajnimgade <pankaj.nimgade@gmail.com>
2016-01-28 11:15:35 +0530

    an layout example made to answer a question on stackOverflow

--- app/src/main/java/google/map/list/activities/GoogleMapTestTwoActivity.java
+++ app/src/main/java/google/map/list/activities/GoogleMapTestTwoActivity.java
@@ -1,17 +1,19 @@
 package google.map.list.activities;
 
 import android.os.Bundle;
-import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 import com.nimgade.pk.mytutorialapplication.R;
 
-public class GoogleMapTestTwoActivity extends Activity {
+public class GoogleMapTestTwoActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_google_map_test_two);
-        getActionBar().setDisplayHomeAsUpEnabled(true);
+
+    }
+
+    private void initializeUI() {
     }
 
 }
