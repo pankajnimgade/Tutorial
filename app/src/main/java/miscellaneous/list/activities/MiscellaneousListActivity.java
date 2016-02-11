31215b18761f75f5a8a3b84fe438a962a80bab8c
pankajnimgade <pankaj.nimgade@gmail.com>
2016-02-11 13:16:44 +0530

    test to check percent support library

--- app/src/main/java/miscellaneous/list/activities/MiscellaneousListActivity.java
+++ app/src/main/java/miscellaneous/list/activities/MiscellaneousListActivity.java
@@ -39,6 +39,7 @@ public class MiscellaneousListActivity extends AppCompatActivity {
         myListItems.add(new MyListItem("Contextual Action Mode Test One", ContextualActionModeTestOneActivity.class));
         myListItems.add(new MyListItem("Create ImageView dynamically", ImageViewDynamicallyActivity.class));
         myListItems.add(new MyListItem("Test Percent One", TestPercentOneActivity.class));
+        myListItems.add(new MyListItem("Test Percent Two", PercentTwoActivity.class));
         ArrayAdapter<MyListItem> adapter =
                 new ArrayAdapter<MyListItem>(getApplicationContext(), R.layout.simple_list_item_1, myListItems);
         listView.setAdapter(adapter);
---
+++ app/src/main/java/miscellaneous/list/activities/PercentTwoActivity.java
@@ -0,0 +1,16 @@
+package miscellaneous.list.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.nimgade.pk.mytutorialapplication.R;
+
+public class PercentTwoActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_percent_two);
+    }
+
+}
